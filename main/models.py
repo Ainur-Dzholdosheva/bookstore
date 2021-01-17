071b3fd12100bdd37f6425401ddcecf9a85bfc29
Ainur-Dzholdosheva <ajnurdzoldoseva@gmail.com>
2021-01-17 22:38:01 +0600

    added to models title subtitles and make migrations

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class BookStore(models.Model):
+    title = models.CharField(max_length=50)
+    subtitle = models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
+    price = models.CharField(max_length=10)
+    genre = models.BooleanField(default=False)
+    author = models.CharField(max_length=20)
+    year = models.BooleanField(default=False)
+    date = models.DateField(auto_now_add=True)
