35c64b499e16dfb911c997793a6e6d97192d0b01
Ainur-Dzholdosheva <ajnurdzoldoseva@gmail.com>
2021-01-19 19:56:12 +0600

    added is_favorite to models

--- main/models.py
+++ main/models.py
@@ -8,4 +8,5 @@ class BookStore(models.Model):
     genre = models.CharField(max_length=20)
     author = models.CharField(max_length=50)
     year = models.CharField(max_length=20)
+    is_favorite = models.BooleanField(default=False)
     date = models.DateTimeField(auto_now_add=True)
