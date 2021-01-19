27e7cc1bcdd1c6130747cc11380911d556509d40
Ainur-Dzholdosheva <ajnurdzoldoseva@gmail.com>
2021-01-19 14:45:07 +0600

    added inputs with description and connected views urls

--- main/views.py
+++ main/views.py
@@ -8,6 +8,12 @@ def homepage(request):
 def add_book(request):
     form = request.POST
     text = form["book_text"]
-    book = BookStore(title=text)
+    subtitle = form["book_subtitle"]
+    description = form["book_description"]
+    price = form["book_price"]
+    genre = form["book_genre"]
+    author = form["book_author"]
+    year = form["book_year"]
+    book = BookStore(title=text, subtitle=subtitle, description=description, price=price, genre=genre, author=author, year=year)
     book.save()
     return redirect(homepage)
